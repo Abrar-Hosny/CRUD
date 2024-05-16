cfca6dff252a0bd9bae1684bc5eb044125c2225a
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-05-16 12:06:44 +0300

    change ids and get elements form a document

--- JS/index.js
+++ JS/index.js
@@ -4,8 +4,16 @@
 
 // 1- create add every thing user write it 
 // we want to take all the input of the user and put it in a json {array of objects }
+// Add product to json [array of object]
 
 
+// 1- get input elements
 
-// 1- inputs tags
+var productName  = document.getElementById("productName");
+var productPrice = document.getElementById("productPrice");
+var productCategorie = document.getElementById("productCategorie");
+var productDescription = document.getElementById("productDescription");
+var productImage=document.getElementById("productImage")
 
+// test every step
+// console.log(productName,productPrice,productCategorie,productDescription,productImage);
