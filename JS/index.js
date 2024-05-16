73d72d7316131d1bd41a17af89f20bffe415b1ef
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-05-16 11:57:19 +0300

    button of add product has been added

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+//  Json => javascript object notation 
+// array of objects 
+
+
+// 1- create add every thing user write it 
+// we want to take all the input of the user and put it in a json {array of objects }
+
+
+
+// 1- inputs tags
+
