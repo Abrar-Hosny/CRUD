0eebdad4262e5fdf44b6a056c176fbb7895fb803
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-06-20 14:34:53 +0300

    crud using local storage finished addproduct , display and delete and search

--- JS/index.js
+++ JS/index.js
@@ -13,63 +13,117 @@ var productName  = document.getElementById("productName");
 var productPrice = document.getElementById("productPrice");
 var productCategorie = document.getElementById("productCategorie");
 var productDescription = document.getElementById("productDescription");
-var productImage=document.getElementById("productImage")
+var productImage=document.getElementById("productImage");
 var displayProduct =document.getElementById("display");
-// test every step
-// console.log(productName,productPrice,productCategorie,productDescription,productImage);
+var searchInput= document.getElementById("search");
 
-// cartoon
-var productList =[];
+var productList;
+
+
+if(localStorage.getItem("products")==null){
+    productList=[] ; 
+}
+else{
+productList = JSON.parse(localStorage.getItem("products")) ; 
+display();
+
+}
+
+
 
 function addProduct(){
-    productList.push({productname:productName.value , 
-        productprice:productPrice.value, 
-        productcategorie:productCategorie.value , 
-        productdescription:productDescription.value,
-        productimage:productPrice.value
-            });
-console.log(productList)
+var nameProduct = productName.value;
+var priceProduct = productPrice.value;
+var categorieProduct = productCategorie.value;
+var descriptionProduct = productDescription.value;
+//image
+
+productList.push({
+    code:nameProduct , 
+    price : priceProduct , 
+    description : descriptionProduct , 
+    categorie : categorieProduct,
+    image : `crudImages/${productImage.files[0].name}`
+});
+localStorage.setItem("products" , JSON.stringify(productList));
+display() ; 
 clearInputs();
+console.log(productList);
 
 }
 
-// the function only one task 
-// 2- clear inputs
+
+
 function clearInputs(){
-    display();
-
-    productName.value =null;
-    productPrice.value =null;
-    productCategorie.value =null;
-    productDescription.value =null;
-productImage.value=null; 
+     productName.value = null ; 
+       productPrice.value = null; 
+       productCategorie.value = null; 
+       productDescription.value = null;
+       productImage.value=null; 
+       
 }
-
-
-// 3- display
-
 
 function display(){
-    for(var i = 0 ; i<productList.length;i++){
-
-displayProduct.innerHTML=`
-<div class="card" style="width: 18rem;">
-  <img src="${productList[i].productimage}" class="card-img-top" alt="productimage">
-  <div class="card-body">
-    <h5 class="card-title">Nameproduct ${productList[i].productname}</h5>
-    <p class="card-text">Price ${productList[i].productprice}</p>
-    <p class="card-text">Categorie ${productList[i].productcategorie}</p>
-    <p class="card-text">Description ${productList[i].productdescription}</p>
-   
-  </div>
-
- 
-</div>
-
-`
-
-    }
+var cartona="" ; 
+for(var i = 0 ; i  < productList.length ; i++){
+    cartona+=`  <div class="col-md-3">
+        <div class="item border border-2 ">
+          <img src="${productList[i].image}" width="100%">
+          <p>Name : ${productList[i].code}</p>
+          <p>Price : ${productList[i].price} </p>
+          <p> categorie : ${productList[i].categorie}</p>
+          <p>Description ${productList[i].description}</p>
+       <button class="btn btn-outline-danger" onclick="deleteProduct(${i})">Delete</button>
+        <button class="btn btn-dark">Edit</button>
+          </div>
+       
+      </div>`;
+}
+displayProduct.innerHTML=cartona;
 
 }
 
 
+function deleteProduct(deletedIndex){
+productList.splice(deletedIndex , 1) ; 
+display(); 
+//here we will do override on the list on the local strorage 
+localStorage.setItem("products" , JSON.stringify(productList));
+
+console.log(productList)
+
+}
+
+
+function search(){
+var word = searchInput.value ; 
+var cartonna="";
+for(var j =0 ; j < productList.length ; j ++ ){
+    if((productList[j].code).toLocaleLowerCase().includes(word.toLocaleLowerCase())){
+      cartonna+=`<div class="col-md-3">
+        <div class="item border border-2 ">
+          <img src="images/aaa7fbe423cbd04dd9ec0d29e6c0dc44.jpg" width="100%">
+          <p>Name : ${productList[j].code}</p>
+          <p>Price : ${productList[j].price} </p>
+          <p> categorie : ${productList[j].categorie}</p>
+          <p>Description ${productList[j].description}</p>
+       <button class="btn btn-outline-danger" onclick="deleteProduct(${j})">Delete</button>
+        <button class="btn btn-dark">Edit</button>
+          </div>
+       
+      </div>`;
+
+    }
+    
+
+}
+
+if(cartonna==""){
+    displayProduct.innerHTML=`<h2>No Data </h2>`
+}
+else{
+    displayProduct.innerHTML=cartonna;
+
+}
+
+}
